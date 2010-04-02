1e79f2ce684fce1609c7a86647b2fabf12ffcf85
Rovetz <andy.rovetz@gmail.com>
2010-04-02 17:28:44 +0300

    added products helper to content controller

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -1,7 +1,9 @@
 class ContentController < Spree::BaseController
   rescue_from ActionView::MissingTemplate, :with => :render_404
   caches_page :show, :index
-
+  
+  helper :products
+  
   def show
     render :action => params[:path].join('/')
   end
@@ -10,7 +12,7 @@ class ContentController < Spree::BaseController
     render "cvv", :layout => false
   end  
   
-  def home
+  def home  	
   	@featured_product = Product.first
   	@products = Product.all :limit => 10, :offset => 2
   	render "home", :layout => false
